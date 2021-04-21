e0490521129a1d8f577ab6d8ae50e078b6f993a7
eduna67 <eduna67@gmail.com>
2021-04-21 14:24:09 -0300

    Dividindo o Main.dart em 3 arquivos para facilitar

---
+++ lib/app/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'view/my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo de Crud'),
+    );
+  }
+}

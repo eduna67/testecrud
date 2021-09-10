e31e042e2ebd5a2a4744217e507e336d9b727b3a
eduna67 <eduna67@gmail.com>
2021-09-10 08:49:17 -0300

    Atualizando fontes

--- lib/app/view/menu.dart
+++ lib/app/view/menu.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class MenuPrincipal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(child: Text('menu 1'),);
+    return Drawer(
+      child: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Text('menu teste 1'),
+      ),
+    );
   }
 }
